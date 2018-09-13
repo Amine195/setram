b29d75f2a2800625db4224bfb53d6e660fa29557
Amine195 <gmadhgs@gmail.com>
2018-09-13 15:10:37 +0100

    chart.js

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
+const Comment = require('../../models/Comment');
+const Pasf = require('../../models/Pasf');
+const Constat = require('../../models/Constat');
 
 router.all('/*', (req, res, next)=>{
     req.app.locals.layout = 'admin';
@@ -11,7 +15,15 @@ router.get('/', (req, res)=>{
 });
 
 router.get('/dashboard', (req, res)=>{
-    res.render('admin/dashboard');
+    Post.count().then(postCount=>{
+        Comment.count().then(commentCount=>{
+            Pasf.count().then(pasfCount=>{
+                Constat.count().then(constatCount=>{
+                    res.render('admin/dashboard', {postCount: postCount, commentCount: commentCount, pasfCount: pasfCount, constatCount: constatCount});
+                });
+            });
+        });
+    });
 });
 
 module.exports = router;
